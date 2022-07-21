d86c8b17164af392a7a3ca251727a34593215a94
CEzekannagha <48945110+Sombiri@users.noreply.github.com>
2022-07-21 12:51:32 +0200

    Database modified

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,15 @@ import firebase from 'firebase/app'
 import 'firebase/database'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCxxGyxvifD3Og8L2gU-DPhD6HLczVBl-k",
-    authDomain: "fir-dnasmart.firebaseapp.com",
-    projectId: "fir-dnasmart",
-    storageBucket: "fir-dnasmart.appspot.com",
-    messagingSenderId: "725954949284",
-    appId: "1:725954949284:web:5707b7f6a392d02ccd3631"
-  };
+  apiKey: "AIzaSyB-WFUGKeIg9dxXYtU6__00GL0qdlc37KI",
+  authDomain: "dnasmart-175df.firebaseapp.com",
+  databaseURL: "https://dnasmart-175df-default-rtdb.firebaseio.com",
+  projectId: "dnasmart-175df",
+  storageBucket: "dnasmart-175df.appspot.com",
+  messagingSenderId: "788307986929",
+  appId: "1:788307986929:web:73e620e350b7d82e0e5c50",
+  measurementId: "G-0Q8M84PEMJ"
+};
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
 
